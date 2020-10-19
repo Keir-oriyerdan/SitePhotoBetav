a5034c1fafa43f83a47800532e3bfb348b136bf0
Keir <mcglencoe@hotmail.fr>
2020-10-19 19:30:59 +0200

    React progress - test

---
+++ js/components/Menu.jsx
@@ -0,0 +1,30 @@
+export default function Menu(props) {
+  return (
+    <nav>
+      <div>
+        <div class="navbar">
+          <a href="index.html" title="ImionnSiad">
+            <img class="title" src="../images/assets/logo.png" />
+          </a>
+          <ul class="navbar-top">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="./categories.html">Galleries</a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            <li>
+              <a href="./contact.html">Contact</a>
+            </li>
+            <li>
+              <a href="#">Mentions légales</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
